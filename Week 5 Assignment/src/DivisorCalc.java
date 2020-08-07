48292f2550e420dadfca10e1c96d3be35b977659
bcallen15 <67833609+bcallen15@users.noreply.github.com>
2020-08-06 19:09:37 -0500

    LAb5Ex3

---
+++ Week 5 Assignment/src/DivisorCalc.java
@@ -0,0 +1,37 @@
+//********************************************************
+//	GreatestCommonDenom.java			Author: Ben Callen
+//
+//	Uses Euclid's algorithm to find the largest common
+//	divisor of two positive integers
+//********************************************************
+
+import java.util.Scanner;
+
+public class DivisorCalc {
+	
+	private static int gcd(int num1, int num2) {
+		if (num2==0) {
+			return num1;
+		}
+		return gcd(num2, num1%num2);
+	}
+
+	public static void main(String[] args) {
+		
+		int num1, num2, greatestCommonDivisor;
+		
+		Scanner number = new Scanner(System.in);
+		
+		System.out.println("Enter the first number: ");
+		num1 = number.nextInt();
+		
+		System.out.println("Enter the second number: ");
+		num2 = number.nextInt();
+		
+		greatestCommonDivisor = gcd(num1,num2);
+		
+		System.out.println("The greatest common divisor of " + num1 + " and " + num2 + " = " + greatestCommonDivisor);
+
+	}
+
+}
